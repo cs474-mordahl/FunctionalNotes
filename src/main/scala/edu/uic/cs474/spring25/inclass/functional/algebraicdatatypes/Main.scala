87e4f5031ed49ff34c4e0a6081a8e011177bcaf5
Austin Mordahl <amordahl@uic.edu>
2025-03-17 16:19:07 -0500

    restructure

--- src/main/scala/edu/uic/cs474/spring25/inclass/functional/algebraicdatatypes/Main.scala
+++
@@ -1,5 +0,0 @@
-package edu.uic.cs474.spring25.inclass.functional.algebraicdatatypes
-
-@main
-def main(args: String*) = 
-  println("Hello, world!")
--- src/main/scala/edu/uic/cs474/spring25/inclass/functional/data/ADTs.scala
+++ src/main/scala/edu/uic/cs474/spring25/inclass/functional/data/ADTs.scala
@@ -1,4 +1,4 @@
-package edu.uic.cs474.spring25.inclass.functional.algebraicdatatypes
+package edu.uic.cs474.spring25.inclass.functional.data
 
 import scala.annotation.tailrec
 
@@ -9,21 +9,33 @@ import scala.annotation.tailrec
   *    - PRODUCT TYPES: A logical AND of types
   */
 
-// Mechanism for product types is final case classes
+/* Mechanism for product types is final case classes; note that if you look at
+ * the bytecode, this implements the scala.Product interface. */
 final case class C(x: Int, y: Int)
 
 // Mechanism for sum types: two options
+
+// First is a trait with case classes/objects that extend it.
 trait Color
 case object Red    extends Color
 case object Blue   extends Color
 case object Yellow extends Color
 
+// Second is an enum (new in Scala 3).
 enum Color2(name: String):
   case Red(name: "Red" = "Red")          extends Color2(name)
   case Blue(name: "Blue" = "Blue")       extends Color2(name)
   case Yellow(name: "Yellow" = "Yellow") extends Color2(name)
 
-// ->> desugars to
+// => desugars to
+// format: off
+/*
+sealed trait Color2(name: String)
+final case class Red(name: "Red" = "Red")          extends Color2(name)
+final case class Blue(name: "Blue" = "Blue")       extends Color2(name)
+final case class Yellow(name: "Yellow" = "Yellow") extends Color2(name)
+*/
+// format: on
 
 // ----- SET ------
 enum Set[+T]:
